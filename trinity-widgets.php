ed1360410f6bec3044cb32ac1dd6b26be26373fb
D Scott Baeder <sbaeder@aol.com>
2016-07-23 17:01:34 -0400

    updates for version 2
    Remove Vimeo - replace with WP-Theatre
    Make all readings one field to simplify cut/paste.
    Remove "txt" version

--- trinity-widgets.php
+++ trinity-widgets.php
@@ -5,16 +5,16 @@ Plugin Name: Trinity Widgets
 Plugin URI: 
 Description: Custom widget to output Sunday's information
 Author: 
-Version: 1
+Version: 2.0
 Author URI:
 */
 
-class this_sunday_tng extends WP_Widget {
+class this_sunday extends WP_Widget {
 
 // constructor
 function __construct() {
 // Give widget name here
-parent::__construct(false, $name = __('This Sunday - TNG', 'wp_widget_plugin') );
+parent::__construct(false, $name = __('This Sunday', 'wp_widget_plugin') );
 
 }
 
@@ -24,59 +24,38 @@ function form($instance) {
 
 // Check values
 if( $instance) {
-$ts_tng_date = esc_attr($instance['ts_tng_date']);
-$ts_tng_title = esc_attr($instance['ts_tng_title']);
-$ts_tng_descarea = $instance['ts_tng_descarea'];
-$ts_tng_reading1 = esc_attr($instance['ts_tng_reading1']);
-$ts_tng_reading2 = esc_attr($instance['ts_tng_reading2']);
-$ts_tng_reading3 = esc_attr($instance['ts_tng_reading3']);
-$ts_tng_reading4 = esc_attr($instance['ts_tng_reading4']);
+$ts_date = esc_attr($instance['ts_date']);
+$ts_title = esc_attr($instance['ts_title']);
+$ts_descarea = $instance['ts_descarea'];
+$ts_readings = $instance['ts_readings'];
 } else {
-$ts_tng_date = ' ';
-$ts_tng_title = ' ';
-$ts_tng_descarea = ' ';
-$ts_tng_reading1 = ' ';
-$ts_tng_reading2 = ' ';
-$ts_tng_reading3 = ' ';
-$ts_tng_reading4 = ' ';
+$ts_date = ' ';
+$ts_title = ' ';
+$ts_descarea = ' ';
+$ts_readings = ' ';
+
 }
 ?>
 
 <p>
-<label for="<?php echo $this->get_field_id('ts_tng_date'); ?>"><?php _e('Sundays Date', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('ts_tng_date'); ?>" name="<?php echo $this->get_field_name('ts_tng_date'); ?>" type="text" value="<?php echo $ts_tng_date; ?>" />
+<label for="<?php echo $this->get_field_id('ts_date'); ?>"><?php _e('Sundays Date', 'wp_widget_plugin'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('ts_date'); ?>" name="<?php echo $this->get_field_name('ts_date'); ?>" type="text" value="<?php echo $ts_date; ?>" />
 </p>
 <p>
-<label for="<?php echo $this->get_field_id('ts_tng_title'); ?>"><?php _e('Sundays Title', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('ts_tng_title'); ?>" name="<?php echo $this->get_field_name('ts_tng_title'); ?>" type="text" value="<?php echo $ts_tng_title; ?>" />
+<label for="<?php echo $this->get_field_id('ts_title'); ?>"><?php _e('Sundays Title', 'wp_widget_plugin'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('ts_title'); ?>" name="<?php echo $this->get_field_name('ts_title'); ?>" type="text" value="<?php echo $ts_title; ?>" />
 </p>
 <p>
-<label for="<?php echo $this->get_field_id('ts_tng_descarea'); ?>"><?php _e('Sundays Description:', 'wp_widget_plugin'); ?></label>
-<textarea class="widefat" id="<?php echo $this->get_field_id('ts_tng_descarea'); ?>" name="<?php echo $this->get_field_name('ts_tng_descarea'); ?>" rows="7" cols="20" >
-<?php echo $ts_tng_descarea; ?>
+<label for="<?php echo $this->get_field_id('ts_descarea'); ?>"><?php _e('Sundays Description:', 'wp_widget_plugin'); ?></label>
+<textarea class="widefat" id="<?php echo $this->get_field_id('ts_descarea'); ?>" name="<?php echo $this->get_field_name('ts_descarea'); ?>" rows="7" cols="20" >
+<?php echo $ts_descarea; ?>
 </textarea>
 </p>
 <p>
-<!--
-<label for="<?php echo $this->get_field_id('ts_tng_link'); ?>"><?php _e('Link to readings - <font color="red">NOTE: A "#" character will automatically generate individual links based on the refereces entered below)</font>', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('ts_tng_link'); ?>" name="<?php echo $this->get_field_name('ts_tng_link'); ?>" type="text" value="<?php echo $ts_tng_link; ?>" />
-</p>
--->
-<p>
-<label for="<?php echo $this->get_field_id('ts_tng_reading1'); ?>"><?php _e('Reading #1', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('ts_tng_reading1'); ?>" name="<?php echo $this->get_field_name('ts_tng_reading1'); ?>" type="text" value="<?php echo $ts_tng_reading1; ?>" />
-</p>
-<p>
-<label for="<?php echo $this->get_field_id('ts_tng_reading2'); ?>"><?php _e('Reading #2', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('ts_tng_reading2'); ?>" name="<?php echo $this->get_field_name('ts_tng_reading2'); ?>" type="text" value="<?php echo $ts_tng_reading2; ?>" />
-</p>
-<p>
-<label for="<?php echo $this->get_field_id('ts_tng_reading3'); ?>"><?php _e('Reading #3', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('ts_tng_reading3'); ?>" name="<?php echo $this->get_field_name('ts_tng_reading3'); ?>" type="text" value="<?php echo $ts_tng_reading3; ?>" />
-</p>
-<p>
-<label for="<?php echo $this->get_field_id('ts_tng_reading4'); ?>"><?php _e('Reading #4', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('ts_tng_reading4'); ?>" name="<?php echo $this->get_field_name('ts_tng_reading4'); ?>" type="text" value="<?php echo $ts_tng_reading4; ?>" />
+<label for="<?php echo $this->get_field_id('ts_readings'); ?>"><?php _e('Readings', 'wp_widget_plugin'); ?></label>
+<textarea class="widefat" id="<?php echo $this->get_field_id('ts_readings'); ?>" name="<?php echo $this->get_field_name('ts_readings'); ?>" rows="5" cols="20" >
+<?php echo $ts_readings; ?>
+</textarea>
 </p>
 <?php
 }
@@ -85,14 +64,11 @@ $ts_tng_reading4 = ' ';
 function update($new_instance, $old_instance) {
 $instance = $old_instance;
 // Fields
-$instance['ts_tng_date'] = strip_tags($new_instance['ts_tng_date']);
-$instance['ts_tng_title'] = strip_tags($new_instance['ts_tng_title']);
-// Don't strip these tags in case we want to add additional HTML Stuff here!
-$instance['ts_tng_descarea'] = $new_instance['ts_tng_descarea'];
-$instance['ts_tng_reading1'] = strip_tags($new_instance['ts_tng_reading1']);
-$instance['ts_tng_reading2'] = strip_tags($new_instance['ts_tng_reading2']);
-$instance['ts_tng_reading3'] = strip_tags($new_instance['ts_tng_reading3']);
-$instance['ts_tng_reading4'] = strip_tags($new_instance['ts_tng_reading4']);
+$instance['ts_date'] = strip_tags($new_instance['ts_date']);
+$instance['ts_title'] = strip_tags($new_instance['ts_title']);
+$instance['ts_descarea'] = $new_instance['ts_descarea'];
+$instance['ts_readings'] = $new_instance['ts_readings'];
+
 return $instance;
 }
 
@@ -102,109 +78,32 @@ function widget($args, $instance) {
 extract( $args );
 
 // these are the widget options
-$ts_tng_date = esc_attr($instance['ts_tng_date']);
-$ts_tng_title = esc_attr($instance['ts_tng_title']);
-$ts_tng_descarea = $instance['ts_tng_descarea'];
-$ts_tng_link = esc_attr($instance['ts_tng_link']);
-$ts_tng_reading1 = esc_attr($instance['ts_tng_reading1']);
-$ts_tng_reading2 = esc_attr($instance['ts_tng_reading2']);
-$ts_tng_reading3 = esc_attr($instance['ts_tng_reading3']);
-$ts_tng_reading4 = esc_attr($instance['ts_tng_reading4']);
+$ts_date = esc_attr($instance['ts_date']);
+$ts_title = esc_attr($instance['ts_title']);
+$ts_descarea = $instance['ts_descarea'];
+$ts_link = esc_attr($instance['ts_link']);
+$ts_readings = esc_attr($instance['ts_readings']);
 
 echo $before_widget;
 // Display the widget
 ?>
 <section class="sunday_box">
-<div class="sunday_box_contant ">
-<h3>This Sunday</h3>
-<center><?php echo $ts_tng_date; ?></center>
-<center><?php echo $ts_tng_title; ?></center>
-<br>
-<p><?php echo $ts_tng_descarea; ?></p>
-<br/>
-<span class="text"><strong>Readings:</strong> (click readings to view)<br>
-
-<a href="<?php
-	echo 'http://bible.oremus.org/?vnum=yes&version=nrsv&passage=%0a' . $ts_tng_reading1 .
-		'%0a' . $ts_tng_reading2 .'%0a' .  $ts_tng_reading3 .'%0a' . $ts_tng_reading4 ;
-?>" target="_blank">
-<?php echo $ts_tng_reading1 . "<br>" . $ts_tng_reading2 . "<br>" . $ts_tng_reading3 . "<br>" . $ts_tng_reading4; ?></a>
-
-</span>
-</div>
-
-<br>
-<div class="view_calendar btn_red"> <a href="<?php echo get_page_link(2); ?>">View Our Calendar</a> </div>
+	<div class="sunday_box_content ">
+		<h3>This Sunday</h3>
+		<center style="font-size: 1.5em;line-height: 1.8em;"><?php echo $ts_date; ?></center>
+		<center style="font-size: 1.0em;padding-bottom: 5px;"><?php echo $ts_title; ?></center>
+		<p><?php echo $ts_descarea; ?></p>
+		<?php $ts_readings = str_replace(".", "", $ts_readings); ?>
+		<span class="text">
+			<strong>Readings:</strong> (click readings to view)<br>
+			<a href="<?php	echo 'http://bible.oremus.org/?vnum=yes&version=nrsv&passage=%0a' . 
+				str_replace(";","%0a",$ts_readings) . '" target="_blank">' . 
+				str_replace(";","<br>", $ts_readings) ;  ?>
+			</a>
+		</span>
+	</div>
+	<br>
 </section>
-&nbsp;<br>
-<?php
-
-echo $after_widget;
-
-}
-} //End Class extension to Widget
-
-
-class vimeo_tng extends WP_Widget {
-
-// constructor
-function __construct() {
-// Give widget name here
-parent::__construct(false, $name = __('Vimeo - TNG', 'wp_widget_plugin') );
-}
-
-// widget form creation
-
-function form($instance) {
-
-// Check values
-if( $instance) {
-$vimeo_tng_date = esc_attr($instance['vimeo_tng_date']);
-$vimeo_tng_link = esc_attr($instance['vimeo_tng_link']);
-} else {
-$vimeo_tng_date = ' ';
-$vimeo_tng_link = ' ';
-}
-?>
-
-<p>
-<label for="<?php echo $this->get_field_id('vimeo_tng_date'); ?>"><?php _e('Vimeo Title (text)', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('vimeo_tng_date'); ?>" name="<?php echo $this->get_field_name('vimeo_tng_date'); ?>" type="text" value="<?php echo $vimeo_tng_date; ?>" />
-</p>
-<p>
-<label for="<?php echo $this->get_field_id('vimeo_tng_link'); ?>"><?php _e('Vimeo File number', 'wp_widget_plugin'); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id('vimeo_tng_link'); ?>" name="<?php echo $this->get_field_name('vimeo_tng_link'); ?>" type="text" value="<?php echo $vimeo_tng_link; ?>" />
-</p>
-
-<?php
-}
-
-
-function update($new_instance, $old_instance) {
-$instance = $old_instance;
-// Fields
-$instance['vimeo_tng_date'] = strip_tags($new_instance['vimeo_tng_date']);
-$instance['vimeo_tng_link'] = strip_tags($new_instance['vimeo_tng_link']);
-return $instance;
-}
-
-// display widget
-function widget($args, $instance) {
-extract( $args );
-
-// these are the widget options
-$vimeo_tng_date = esc_attr($instance['vimeo_tng_date']);
-$vimeo_tng_link = esc_attr($instance['vimeo_tng_link']);
-
-echo $before_widget;
-// Display the widget
-?>
-<div class="video_box">
-<iframe src="//player.vimeo.com/video/<?php echo $vimeo_tng_link; ?>" width="WIDTH" height="HEIGHT" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p>
-<a href="http://vimeo.com/<?php echo $vimeo_tng_link; ?>"><?php echo $vimeo_tng_date; ?></a> from <a href="http://vimeo.com/trinityworcester">Trinity Lutheran Worcester</a> on <a href="http://vimeo.com">Vimeo</a>.</p></a> 
-<br/>
-</div>
-
 <?php
 
 echo $after_widget;
@@ -213,5 +112,4 @@ echo $after_widget;
 } //End Class extension to Widget
 
 // register widget
-add_action('widgets_init', create_function('', 'return register_widget("this_sunday_tng");'));
-add_action('widgets_init', create_function('', 'return register_widget("vimeo_tng");'));
+add_action('widgets_init', create_function('', 'return register_widget("this_sunday");'));
